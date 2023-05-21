77d8038f61ec708e16714c3cd0c81b0fd7c5e023
Ilya Koloskov <ilblesskol@gmail.com>
2023-05-21 20:37:04 +0300

    add webpack

---
+++ src/components/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>{{ message }}</h1>
+  <button @click="count++">Count is: {{ count }}</button>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+      message: 'Hello Vue',
+    }
+  },
+}
+</script>
+
+<style scoped>
+button {
+  font-weight: bold;
+}
+</style>
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './components/App.vue'
+
+import './main.sass'
+
+const app = createApp(App).mount('#app')
